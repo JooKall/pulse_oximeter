02777e3c0e684f0525461b8765c1454a82b3304a
Joonas Kallio <kallio.joonas@proton.me>
2025-11-25 11:50:04 +0200

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api/sensor': 'http://localhost:3001'
+      '/api/sensorData': 'http://localhost:3001'
     }
   }
 })
